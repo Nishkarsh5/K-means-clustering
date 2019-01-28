a76cb9461ba30270cd6e9ae88d7aa1d618e41e4f
Ankit Solanki <ankit03june@gmail.com>
2019-01-28 19:59:08 +0530

    ADD new functionality and better method variables

--- Sequential/cluster.h
+++ Sequential/cluster.h
@@ -11,22 +11,27 @@ using namespace std;
 class Cluster{
 
 private:
-	int cluserid;
-	vector<Point> points;
+	int cluserid; //ID of cluster
+	Point clusterCenter; //centroid of cluster
+	vector<Point> points; // all the points in that cluster
 
 public:
 	Cluster(){}
-	Cluster(int id, Point p){
+	Cluster(int id, Point p){ //construct a cluster from id and center
 		this->cluserid = id;
-		points.push_back(p);
+		clusterCenter = p;
 	}
 
-	int getID(){return cluserid;}
-	void addPointToCluster(Point p){
+	int getClusterID(){return cluserid;} //get id
+
+	Point getCenter(){return clusterCenter;} // get centroid
+	void makeCenter(Point c){clusterCenter = c;} //make c centroid
+
+	vector<Point> getAllPoints(){return points;} //get all points in cluster
+	void addPointToCluster(Point p){ //add point to cluster
 		points.push_back(p);
 	}
-
-	bool removePointFromCluster(int id){
+	bool removePointFromCluster(int id){ //remove point from cluster
 		for(int i=0; i<points.size(); i++){
 			if((points.at(i)).id == id){
 				points.erase(points.begin() + i);
@@ -34,10 +39,11 @@ public:
 		}
 	}
 
-	void print(){
+	void print(){ //print the cluster
 		cout<<"ID: "<<cluserid<<endl;
 		cout<<"Points in cluster: "<<points.size()<<endl;
 	}
+
 };
 
 #endif
--- Sequential/point.h
+++ Sequential/point.h
@@ -7,19 +7,36 @@
 using namespace std;
 
 class Point{
-public:
-	int id;
-	tuple<int, int, int> value;
-	int clusterid;
+private:
+	int id; //id of the point
+	tuple<int, int, int> value; //value
+	int clusterid; //which cluster he is in?
 
+public:
 	Point(){}
-	Point(int i, tuple<int, int, int> v){this->id = i; this->value = v;}
-		
-	void setCluster(int i){ this->clusterid = i;}
-	void print(){
+	Point(int i, tuple<int, int, int> v){
+		this->id = i;
+		this->value = v;
+		this->clusterid = -1;
+	} //construct
+	
+	int getID(){return id;} //get id of point
+	int getClusterID(return clusterid;) //get cluster id it belongs to
+	tuple<int, int, int> getValue(){
+		return value;
+	}
+
+	void setCluster(int i){ this->clusterid = i;} //set cluster id
+	void print(){ //print the shit
 		cout<<"[.] ID: "<<id<<endl;
 		cout<<"[.] Value: "<<"("<<get<0>(value)<<", "<<get<1>(value)<<", "<<get<2>(value)<<")"<<endl;
 		cout<<"[.] ClusterID: "<<clusterid<<endl;
+	}
+
+	float eucledianDistanceFrom(const Point &p){
+		tuple<int, int, int> v1 = this->value;
+		tuple<int, int, int> v2 = p.getValue();
+		return sqrt(pow((get<0>(v1) - get<0>(v2), 2)) + pow((get<1>(v1) - get<1>(v2), 2)) + pow((get<2>(v1) - get<2>(v2), 2)));
 	}
 };
 
