7b73ec56f0bd1c50b3a20b1252a498e97ab7106e
Ankit Solanki <ankit03june@gmail.com>
2019-01-29 20:23:17 +0530

    EDIT cluster features and ADD bash files

--- Sequential/cluster.h
+++ Sequential/cluster.h
@@ -10,15 +10,17 @@ using namespace std;
 class Cluster{
 
 private:
+	int dim;
 	int clusterid; //ID of cluster
 	Point clusterCenter; //centroid of cluster
-	vector<Point> points; // all the points in that cluster
+	vector<Point*> points; // all the points in that cluster
 
 public:
 	Cluster(){}
-	Cluster(int id, Point p){ //construct a cluster from id and center
+	Cluster(int id, Point p, int d=3){ //construct a cluster from id and center
 		this->clusterid = id;
 		clusterCenter = p;
+		dim = d;
 	}
 
 	int getClusterID(){return clusterid;} //get id
@@ -26,19 +28,40 @@ public:
 	Point getCenter(){return clusterCenter;} // get centroid
 	void makeCenter(const Point &c){clusterCenter = c;} //make c centroid
 
-	vector<Point> getAllPoints(){return points;} //get all points in cluster
-	void addPointToCluster(Point p){ //add point to cluster
+	vector<Point*> getAllPoints(){return points;} //get all points in cluster
+	void addPointToCluster(Point* p){ //add point to cluster
 		points.push_back(p);
 	}
 
 	bool removePointFromCluster(int id){ //remove point from cluster
+		int i1 = points.size();
 		for(int i=0; i<points.size(); i++){
-			if((points.at(i)).getID() == id){
+			if((points.at(i))->getID() == id){
 				points.erase(points.begin() + i);
-				return true;
 			}
 		}
-		return false;
+		int i2 = points.size();
+		if((i1-i2)!=1){
+			cout<<i1-i2<<endl;
+			return false;
+		}
+		return true;
+	}
+
+	void makeCentroidCenter(){ //take average of all values at make it center
+		vector<int> sum(dim, 0);
+
+		for(int i=0; i<points.size(); i++){
+			for(int k=0; k<dim; k++){
+				sum.at(k) += (points.at(i)->getValue()).at(k);
+			}
+		}
+
+		for(int k=0; k<sum.size(); k++){
+			sum.at(k)/=dim;
+		}
+
+		makeCenter(Point(-1, sum));
 	}
 
 	void print(){ //print the cluster
@@ -47,7 +70,7 @@ public:
 		clusterCenter.print();
 		
 		for(int i=0; i<points.size(); i++){
-			(points.at(i)).print();
+			(points.at(i))->print();
 		}
 		cout<<"--------------------------------\n";
 	}
--- Sequential/run.sh
+++ Sequential/run.sh
@@ -1,2 +1,3 @@
 g++ -o test main.cpp -std=c++11
 ./test
+python3 plot.py
